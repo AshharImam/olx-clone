63af2dc79dca6200456062cf2edae1a30c2523dc
Ashhar Imam <ashharimam490@gmail.com>
2020-11-05 08:40:05 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
